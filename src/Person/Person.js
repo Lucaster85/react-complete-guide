e2dcb893a05c84a42c2cb38e0861684c75c88355
Lucaster85 <lucasdc87@gmail.com>
2020-09-04 20:53:03 -0300

    clean code of examples

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,34 +1,16 @@
 import React from 'react';
-//import Radium from 'radium';
-import styled from 'styled-components';
 import './Person.css';
 
-const StyledDiv = styled.div`
-                    width: 60%;
-                    margin: 16px auto;
-                    border: 1px solid #eee;
-                    box-shadow: 0 2px 3px #ccc;
-                    padding: 16px;
-                    text-align: center;
-                    @media (min-width: 500px): {
-                        width: 450px;
-                    }
-                    `;
-
 const person = props => {
-    /* const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    } */
+    
     return (
-    <StyledDiv>
-        {/* <div className="Person" style={style}> */}
+    
+        <div className="Person" >
         <p onClick={props.click}>Soy {props.name} y tengo {props.age} años.</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
-        {/* </div> */}
-    </StyledDiv>
+        </div>
+    
     );
 }
 
